7e8ce5a23503ff37c8a762cf76bf517c92ea7a52
Paul Glushak <episodex@gmail.com>
2019-03-28 16:48:32 +0200

    Initial GH commit (1.1.4)
    Initial GH commit (1.1.4)

---
+++ readme.txt
@@ -0,0 +1,72 @@
+=== Shortcodes for Yotpo ===
+Contributors: hxii
+Tags: yotpo, shortcode, shortcodes, yotpo add-on, yotpo shortcodes, shortcodes for yotpo
+Requires at least: 4.6
+Requires PHP: 5.6
+Tested up to: 5.2
+Stable tag: 1.1.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds the ability to use shortcodes to control the placement of Yotpo widgets.
+
+== Description ==
+= Prerequisites (Important) =
+* The Yotpo plugin must be installed (obviously).
+* WooCommerce 3.X and above (due to new methods).
+This plugin allows using shortcodes to display Yotpo widgets inside and oustide (applicable widgets only) of product pages e.g. page builders, sidebars, widgets etc.
+
+** For updates & usage instructions: [http://paulglushak.com/shortcodes-for-yotpo/](http://paulglushak.com/shortcodes-for-yotpo/) **
+= Usage =
+`
+[yotpo_widget]
+[yotpo_bottomline product_id="47" noempty]
+[yotpo_product_gallery gallery_id="5bbb561f5ea79223a9da0e7c"]
+[yotpo_product_reviews_carousel background_color="#22fff2" mode="most-recent" type="both" count="3"]
+[yotpo_badge]
+[yotpo_testimonials]
+`
+For the reviews carousel, get the attributes when generating the code from your Yotpo dashboard.
+Special arguments exclusive to this plugin:
+1. **yotpo_product_gallery** - adding `noproduct` will prevent a product ID being added if the gallery is not a product gallery.
+2. **yotpo_product_reviews_carousel**:
+adding `product_id="product-id-here"` will display a reviews carousel for the given product id.
+adding `noproduct` will prevent a product id from being added in case you need a reviews carousel for all reviews.
+3. **yotpo_widget** and **yotpo_bottomline** accept an optional `product_id` argument (e.g. `product_id="47") if you'd like to provide a product ID. Otherwise, the ID of the current product will be used.
+
+== Installation ==
+Installation is very simple:
+1. Upload the plugin files to the `/wp-content/plugins/yotpo-shortcodes` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Use the shortcodes throughout your store.
+
+== Frequently Asked Questions ==
+= A shortcode is missing! / I need more shortcodes! =
+Get in touch, and I'll see what can be done.
+
+== Screenshots ==
+1. Example usage of the shortcodes.
+== Changelog ==
+= 1.1.4 =
+* OOP Rewrite.
+* Fixed requirement of Yotpo.
+* Checked with Wordpress 5.2 alpha.
+= 1.1.3 =
+* Fixed issue with `[yotpo_bottomline]` when arguments are not given.
+= 1.1.2 =
+* Added `[yotpo_testimonials]` shortcode.
+* Added empty template to `[yotpo_bottomline]` (when product has no reviews).
+* Tested with latest versions of Wordpress and WooCommerce
+= 1.1.1 =
+* Added manual mode to reviews carousel.
+* Checked with latest versions of Wordpress and WooCommerce.
+= 1.1 =
+* Added badge widget.
+* Added optional `product_id` arguments to main widget and bottomline widget.
+* Now returning HTML instead of echoing yotpo functions.
+* Make sure to return nothing if post is not a product and no ID is supplied.
+= 1.0.1 =
+* Added `noproduct` arguments.
+* Initial release on WP.
+= 1.0 =
+* Initial commit.
---
+++ wc_yotpo_shortcodes.php
@@ -0,0 +1,180 @@
+<?php
+/*
+* Plugin Name: Shortcodes for Yotpo
+* Description: This plugin adds the ability to use shortcodes to control the placement of Yotpo widgets.
+* Version: 1.1.4
+* Author: Paul Glushak
+* Author URI: http://paulglushak.com/
+* Plugin URI: http://paulglushak.com/shortcodes-for-yotpo/
+* WC requires at least: 3.1.0
+* WC tested up to: 3.6.0
+*/
+
+/*
+ * This plugin allows using shortcodes to display Yotpo widgets inside and oustide (applicable widgets only) of product pages e.g. page builders, sidebars, widgets etc.
+ * See example usage at the bottom.
+*/
+
+// if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Shortcodes!
+ */
+class Yotpo_Shortcodes
+{
+
+	function __construct()
+	{
+		$this->init_shortcodes();
+	}
+
+	function init_shortcodes() {
+		add_shortcode( 'yotpo_widget', array( $this, 'yotpo_widget' ) );
+		add_shortcode( 'yotpo_bottomline', array( $this, 'yotpo_bottomline' ) );
+		add_shortcode( 'yotpo_product_gallery', array( $this, 'yotpo_product_gallery' ) );
+		add_shortcode( 'yotpo_product_reviews_carousel', array( $this, 'yotpo_product_reviews_carousel' ) );
+		add_shortcode( 'yotpo_badge', array( $this, 'yotpo_badge' ) );
+		add_shortcode( 'yotpo_testimonials', array( $this, 'yotpo_testimonials' ) );
+	}
+	/**
+	 * Basic dependency check, to be replaced with class requirement
+	 *
+	 * @return void
+	 **/
+	private function basic_check() {
+		if ( !class_exists( 'woocommerce' ) ) {
+			return;
+		} elseif ( !function_exists( 'wc_yotpo_get_product_data' ) ) {
+			require_once( ABSPATH . 'wp-content/plugins/yotpo-social-reviews-for-woocommerce/wc_yotpo.php' );
+		}
+	}
+
+	public function yotpo_widget( $args ) {
+		$this->basic_check();
+		if ( isset( $args['product_id'] ) ) { $product_id = $args['product_id']; } elseif ( is_product() ) { global $product; $product_id = $product->get_id(); } else { return; }
+		$_product = wc_get_product( $product_id );
+		if ( is_null( $_product ) || !$_product ) { return; }
+		$widget_product_data = wc_yotpo_get_product_data( $_product );
+		$html = "<div class='yotpo yotpo-main-widget'
+	   				data-product-id='{$product_id}'
+	   				data-name='{$widget_product_data['title']}' 
+	   				data-url='{$widget_product_data['url']}' 
+	   				data-image-url='{$widget_product_data['image-url']}' 
+	  				data-description='{$widget_product_data['description']}' 
+	  				data-lang='{$widget_product_data['lang']}'
+	                data-price='{$_product->get_price()}'
+	                data-currency='".get_woocommerce_currency()."'></div>";
+		return $html;
+	}
+
+	public function yotpo_bottomline( $args ) {
+		$this->basic_check();
+		if ( !class_exists( 'Yotpo' ) ) { require_once( ABSPATH . 'wp-content/plugins/yotpo-social-reviews-for-woocommerce/lib/yotpo-api/Yotpo.php' ); }
+		$yotpo_settings = get_option( 'yotpo_settings' );
+		if ( isset( $args['product_id'] ) ) {
+			$product_id = $args['product_id'];
+		} elseif ( is_product() ) {
+			global $product;
+			$product_id = $product->get_id();
+		} else {
+			return;
+		}
+		$data = array();
+		$data['product_id'] = $product_id;
+		$data['app_key'] = $yotpo_settings['app_key'];
+		$yotpo = new Yotpo( $yotpo_settings['app_key'], $yotpo_settings['secret'] );
+		$response = $yotpo->get_product_bottom_line( $data );
+		if ( !empty( $response ) ) {
+			if ( $response['status']['code'] && $response['response']['bottomline']['total_reviews'] > 0 ) {
+				$widget_product_data = wc_yotpo_get_product_data( wc_get_product( $product_id ) );
+				$html = "<div class='yotpo bottomLine'
+							data-product-id='{$product_id}'
+			   				data-url='{$widget_product_data['url']}'
+			   				data-lang='{$widget_product_data['lang']}'>
+		   				</div>";
+			} elseif ( ( $response['response']['bottomline']['total_reviews'] == 0 ) && ( !isset( $args['0'] ) || $args['0'] != "noempty" ) ) {
+				$html = "<div class='yotpo bottomline'>
+							<div class='yotpo-bottomline pull-left star-clickable'>
+								<span class='yotpo-stars'>
+									<span class='yotpo-icon yotpo-icon-empty-star pull-left'></span>
+									<span class='yotpo-icon yotpo-icon-empty-star pull-left'></span>
+									<span class='yotpo-icon yotpo-icon-empty-star pull-left'></span>
+									<span class='yotpo-icon yotpo-icon-empty-star pull-left'></span>
+									<span class='yotpo-icon yotpo-icon-empty-star pull-left'></span>
+								</span>
+								<div class='yotpo-clr'></div>
+							</div>
+						</div><br>";
+			} else {
+				return;
+			}
+		}
+		return $html;
+	}
+
+	public function yotpo_product_gallery( $args ) {
+		$this->basic_check();
+		if ( empty( $args['gallery_id'] ) ) { return 'Error - no gallery ID specified'; }
+		$html = "<div class='yotpo yotpo-pictures-widget' data-gallery-id='{$args['gallery_id']}'";
+		if ( ( !isset($args[0]) || $args[0] != 'noproduct' ) && is_product() ) {
+			global $product;
+			$html .= "data-product-id='{$product->get_id()}'";
+		} elseif ( array_key_exists('product_id', $args) ) {
+			$html .= "data-product-id='{$args['product_id']}'";
+		}
+		$html .= "></div>";
+		return $html;
+	}
+
+	public function yotpo_product_reviews_carousel( $args ) {
+		$this->basic_check();
+		extract( shortcode_atts( array(
+			'background_color' => 'transparent', // transparent or #color
+			'mode' => 'top_rated', // top_rated or most_recent
+			'type' => 'per_product', // per_product, product, both or site
+			'count' => '9', // 3-9
+			'show_bottomline' => '1', 
+			'autoplay_enabled' => '1',
+			'autoplay_speed' => '3000',
+			'show_navigation' => '1'), $args ) );
+		$html = "<div
+			class='yotpo yotpo-reviews-carousel'
+			data-background-color='{$background_color}' 
+			data-mode='{$mode}' 
+			data-type='{$type}' 
+			data-count='{$count}' 
+			data-show-bottomline='{$show_bottomline}' 
+			data-autoplay-enabled='{$autoplay_enabled}' 
+			data-autoplay-speed='{$autoplay_speed}' 
+			data-show-navigation='{$show_navigation}'";
+		if ( isset( $args['product_id'] ) ) {
+			$html .= "data-product-id='{$args['product_id']}'";
+		} elseif ( $mode == 'manual' && isset( $args['review-ids'] ) ) {
+			$html .= "data-review-ids='{$args['review-ids']}'";
+		} elseif ( isset($args[0]) && $args[0] == 'noproduct' ) {
+			$html .= "";
+		} elseif ( is_product() ) {
+			global $product;
+			$html .= "data-product-id='{$product->get_id()}'";
+		} else {
+			return;
+		}
+		$html .= '></div>';
+		return $html;
+	}
+
+	public function yotpo_badge() {
+		$html = "<div id='y-badges' class='yotpo yotpo-badge badge-init'>&nbsp;</div>";
+		return $html;
+	}
+
+	public function yotpo_testimonials() {
+		$html = "<div id='yotpo-testimonials-custom-tab'></div>";
+		return $html;
+	}
+}
+
+new Yotpo_Shortcodes;
+
+?>
